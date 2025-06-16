4c60b072dd387019701f6802c0c98b543a8d2c38
plcrisp <pedrolcrisp@gmail.com>
2025-06-16 20:48:13 -0300

    ➕ importing fortawesome

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
+import './index.css';
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faUser, faLock } from '@fortawesome/free-solid-svg-icons';
+library.add(faUser, faLock);
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
