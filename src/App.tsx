5121c8284150e4416f6a243f803c0e80517f3131
plcrisp <pedrolcrisp@gmail.com>
2025-07-07 21:41:38 -0300

    🚀 reward and dashboard routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage/LoginPage";
+import DashboardPage from "./pages/DashboardPage/DashboardPage";
+import RewardsPage from "./pages/RewardsPage/RewardsPage";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-      </Routes>
-    </Router>
-  )
+    <Routes>
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/dashboard" element={<DashboardPage />} />
+      <Route path="/recompensas" element={<RewardsPage />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
